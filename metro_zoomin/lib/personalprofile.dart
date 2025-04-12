47f949954d550e0435be853583d8fe6fafe3091a
melswain <melanie.l.swain@gmail.com>
2025-04-12 13:51:15 -0400

    Somewhat implemented screen flow (all classes?)

---
+++ metro_zoomin/lib/personalprofile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PersonalProfile extends StatefulWidget {
+  const PersonalProfile({super.key});
+
+  @override
+  State<PersonalProfile> createState() => _PersonalProfileState();
+}
+
+class _PersonalProfileState extends State<PersonalProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
